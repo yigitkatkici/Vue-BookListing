f534347052050b662d615f2b6482bf818c5f31d3
Yigit Katkici <yigit.katkici@libertymutual.com>
2020-12-16 15:08:29 -0500

    Test Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import BookList from './components/HelloWorld';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    BookList,
   },
 };
 </script>
---
+++ src/components/BookList.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1>
+        <title>
+            
+        </title>
+    </h1>
+</div>
+    
+</template>
+
+<script>
+
+name= "Book List"
+
+export default function test() {
+    name = "Booklust";
+    title = "All Books";
+    return title;
+}
+</script>
+
+<style scoped>
+h1 {
+    font-weight: normal;
+}
+h2 { 
+    font-weight: normal;
+}
+</style>
